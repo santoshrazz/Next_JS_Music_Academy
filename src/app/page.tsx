e4c6c7337c321acca879b3e598e25c9dcfcb8f86
santoshrazz <santoshrajbgp11@gmail.com>
2024-05-25 11:40:05 +0530

    added why choose us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { HeroSection } from "@/Components/HeroSection/HeroSection";
 import { Mask_Reveal } from "@/Components/Mask_Reveal/Mask_Reveal";
 import Courses from "@/Components/Our_Courses/Courses";
+import { WhyChooseUs } from "@/Components/WhyChooseUs/WhyChooseUs";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Mask_Reveal />
       <Courses />
+      <WhyChooseUs />
     </>
   );
 }
