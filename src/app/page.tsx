baa737e2a4a42738a018938b18215e07b94f892d
santoshrazz <santoshrajbgp11@gmail.com>
2024-05-25 10:19:43 +0530

    added mask reveal and featured courses component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { HeroSection } from "@/Components/HeroSection/HeroSection";
+import { Mask_Reveal } from "@/Components/Mask_Reveal/Mask_Reveal";
+import Courses from "@/Components/Our_Courses/Courses";
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <HeroSection />
+      <Mask_Reveal />
+      <Courses />
+    </>
+  );
 }
