d53243b4476f4c65a3cf2f9e861c70d05a5c0ecf
santoshrazz <santoshrajbgp11@gmail.com>
2024-06-14 21:43:31 +0530

    again change tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,15 @@
-import { Config } from "tailwindcss";
-import defaultTheme from "tailwindcss/defaultTheme";
-import colors from "tailwindcss/colors";
-import svgToDataUri from "mini-svg-data-uri";
-// import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
-import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 // Tailwind plugin - 1
-function addVariablesForColors({
-  addBase,
-  theme,
-}: {
-  addBase: any;
-  theme: any;
-}) {
-  const allColors = flattenColorPalette(theme("colors"));
-  const newVars = Object.fromEntries(
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
@@ -23,26 +19,20 @@ function addVariablesForColors({
 }
 
 // Tailwind plugin - 2
-function addSvgPatterns({
-  matchUtilities,
-  theme,
-}: {
-  matchUtilities: any;
-  theme: any;
-}) {
+function addSvgPatterns({ matchUtilities, theme }: any) {
   matchUtilities(
     {
-      "bg-grid": (value: string) => ({
+      "bg-grid": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
         )}")`,
       }),
-      "bg-grid-small": (value: string) => ({
+      "bg-grid-small": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
         )}")`,
       }),
-      "bg-dot": (value: string) => ({
+      "bg-dot": (value: any) => ({
         backgroundImage: `url("${svgToDataUri(
           `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
         )}")`,
@@ -102,5 +92,4 @@ const config: Config = {
   },
   plugins: [addVariablesForColors, addSvgPatterns],
 };
-
 export default config;
